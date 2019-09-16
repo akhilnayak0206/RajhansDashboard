6f3b018e502d9aef2efb595197317110657bdb4b
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2019-09-16 14:16:52 +0530

    account working

--- src/views/DashboardScreens/Account.js
+++ src/views/DashboardScreens/Account.js
@@ -1,40 +1,55 @@
-import React, { Component } from 'react'
-import { Switch, Card, Icon, Avatar, Input } from "antd";
+import React, { Component } from "react";
+import { Switch, Card, Icon, Avatar, Input, Button, Skeleton } from "antd";
 import { NavLink } from "react-router-dom";
 import firebase from "../../config/fbConfig";
 import "../../styles/Account.css";
 
 const { Meta } = Card;
 class Account extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      loading:false,
-      email:"",
-      name:""
-    }
+    this.state = {
+      loading: false,
+      loadingSign:false,
+      email: "",
+      name: ""
+    };
   }
   render() {
     return (
       <div>
         <h1 className="heading">Account Settings</h1>
         <div className="mainDiv">
-        <Card className="cardBoth" style={{margin:16}} loading={this.state.loading}>
-          <h3>Email</h3>
-          <Input placeholder="Email is being Loaded" value={this.state.email} />
-          <h3>Name</h3>
-          <Input placeholder="Name is being Loaded" value={this.state.email} />
-        </Card>
-        <Card className="cardBoth" style={{margin:16}} loading={this.state.loading}>
-          <h3 style={{textAlign:'center'}} >Jai Mitra Mandal</h3>
-          <p>This website is made by Akhil Nayak if you have any doubt or suggestion you can contact
-          me by emailing me to nnewn3@gmail.com
-          </p>
-        </Card>
+          <Card className="cardBoth" style={{ margin: 5 }}>
+            <Skeleton loading={this.state.loading} active={true} >
+              <h3>Email</h3>
+              <Input
+                placeholder="Email is being Loaded"
+                value={this.state.email}
+              />
+              <h3>Name</h3>
+              <Input
+                placeholder="Name is being Loaded"
+                value={this.state.name}
+              />
+            </Skeleton>
+          </Card>
+          <Card className="cardBoth" style={{ margin: 5}}>
+            <Skeleton loading={this.state.loadingSign} paragraph={{ rows: 3 }} >
+              <h3 style={{ textAlign: "center" }}>Jai Mitra Mandal</h3>
+              <p style={{ color: "rgba(0,0,0,0.5)" }}>
+                This website is made by Akhil Nayak if you have any doubt or
+                suggestion you can contact me by emailing me to nnewn3@gmail.com
+              </p>
+              <div style={{ textAlign: "center" }}>
+                <Button type="danger">Sign Out</Button>
+              </div>
+            </Skeleton>
+          </Card>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Account
+export default Account;
