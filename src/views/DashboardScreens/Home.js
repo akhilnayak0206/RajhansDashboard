0c31e91926c28dd9d50313a01a5caccac75ba09f
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2019-12-03 15:12:10 +0530

    font and card added

--- src/views/DashboardScreens/Home.js
+++ src/views/DashboardScreens/Home.js
@@ -1,13 +1,157 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { OnAuth } from "../../store/actions/actions";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
+import { Card, Skeleton } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRupeeSign } from "@fortawesome/free-solid-svg-icons";
+import "../../styles/Account.css";
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <p>Home</p>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div
+          style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}
+        >
+          <Card
+            style={{
+              flex: "40%",
+              margin: 5,
+              textAlign: "center",
+              fontFamily: "'Open Sans', sans-serif"
+            }}
+            title="Total"
+          >
+            <Skeleton loading={false} paragraph={{ rows: 3 }}>
+              <h1
+                style={{
+                  textAlign: "center",
+                  fontFamily: "'Open Sans', sans-serif",
+                  fontWeight: "bold",
+                  fontSize: "30px"
+                }}
+              >
+                <FontAwesomeIcon icon={faRupeeSign} />
+                1234
+              </h1>
+            </Skeleton>
+          </Card>
+          <Card
+            style={{
+              flex: "40%",
+              margin: 5,
+              textAlign: "center",
+              fontFamily: "'Open Sans', sans-serif"
+            }}
+            title="Cash In Hand"
+          >
+            <Skeleton loading={false} paragraph={{ rows: 3 }}>
+              <h1
+                style={{
+                  textAlign: "center",
+                  fontFamily: "'Open Sans', sans-serif",
+                  fontWeight: "bold",
+                  fontSize: "30px"
+                }}
+              >
+                <FontAwesomeIcon icon={faRupeeSign} />
+                1234
+              </h1>
+            </Skeleton>
+          </Card>
+          <Card
+            style={{
+              flex: "40%",
+              margin: 5,
+              textAlign: "center",
+              fontFamily: "'Open Sans', sans-serif"
+            }}
+            title="Collected"
+          >
+            <Skeleton loading={false} paragraph={{ rows: 3 }}>
+              <h1
+                style={{
+                  textAlign: "center",
+                  fontFamily: "'Open Sans', sans-serif",
+                  fontWeight: "bold",
+                  fontSize: "30px"
+                }}
+              >
+                <FontAwesomeIcon icon={faRupeeSign} />
+                1234
+              </h1>
+            </Skeleton>
+          </Card>
+          <Card
+            style={{
+              flex: "40%",
+              margin: 5,
+              textAlign: "center",
+              fontFamily: "'Open Sans', sans-serif"
+            }}
+            title="Expense"
+          >
+            <Skeleton loading={false} paragraph={{ rows: 3 }}>
+              <h1
+                style={{
+                  textAlign: "center",
+                  fontFamily: "'Open Sans', sans-serif",
+                  fontWeight: "bold",
+                  fontSize: "30px"
+                }}
+              >
+                <FontAwesomeIcon icon={faRupeeSign} />
+                1234
+              </h1>
+            </Skeleton>
+          </Card>
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <Card style={{ margin: 5 }}>
+            <Skeleton paragraph={{ rows: 3 }}>
+              <h3 style={{ textAlign: "center" }}>Jai Mitra Mandal</h3>
+              <p style={{ color: "rgba(0,0,0,0.5)" }}>
+                This website is made by Akhil Nayak if you have any doubt or
+                suggestion you can contact me by emailing me to nnewn3@gmail.com
+              </p>
+            </Skeleton>
+          </Card>
+          <Card style={{ margin: 5 }}>
+            <Skeleton paragraph={{ rows: 3 }}>
+              <h3 style={{ textAlign: "center" }}>Jai Mitra Mandal</h3>
+              <p style={{ color: "rgba(0,0,0,0.5)" }}>
+                This website is made by Akhil Nayak if you have any doubt or
+                suggestion you can contact me by emailing me to nnewn3@gmail.com
+              </p>
+            </Skeleton>
+          </Card>
+          <Card style={{ margin: 5 }}>
+            <Skeleton paragraph={{ rows: 3 }}>
+              <h3 style={{ textAlign: "center" }}>Jai Mitra Mandal</h3>
+              <p style={{ color: "rgba(0,0,0,0.5)" }}>
+                This website is made by Akhil Nayak if you have any doubt or
+                suggestion you can contact me by emailing me to nnewn3@gmail.com
+              </p>
+            </Skeleton>
+          </Card>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Home
+function mapStateToProps(state) {
+  const { firebase, auth } = state;
+  return {
+    firebase,
+    auth
+  };
+}
+
+export default compose(
+  connect(mapStateToProps, {
+    OnAuth
+  })
+)(Home);
